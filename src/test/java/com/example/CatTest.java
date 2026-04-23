9c48b853cf03fb857128744e9564e19afcc49f44
Nina Kalashnikova <nnklshnkv@yandex.ru>
2026-04-23 15:46:43 +0300

    Fix CatTest: move initialization to @Before

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -16,10 +17,15 @@ public class CatTest {
 
     @Mock
     private Feline felineMock;
+    private Cat cat;
+
+    @Before
+    public void setUp() {
+        cat = new Cat(felineMock);
+    }
 
     @Test
     public void catGetSound() {
-        Cat cat = new Cat(felineMock);
         assertEquals("Мяу", cat.getSound());
     }
 
@@ -27,7 +33,6 @@ public class CatTest {
     public void catGetFood() throws Exception {
         Mockito.when(felineMock.eatMeat()).thenReturn(Arrays.asList("Животные", "Птицы", "Рыба"));
 
-        Cat cat = new Cat(felineMock);
         List<String> food = cat.getFood();
 
         assertEquals(Arrays.asList("Животные", "Птицы", "Рыба"), food);
